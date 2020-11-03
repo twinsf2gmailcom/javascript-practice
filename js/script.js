ddfba3dc4e3eeb53dc9b67480f113bbd93da028c
Stephanie Flora <twinsf2@gmail.com>
2020-11-02 19:34:16 -0800

    take-home homework

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Day 1
+let objOne = {
+    name: 'Stephanie',
+    favfood: "homemade peach ice cream",
+}
+
+console.log('My name is ' + objOne.name)
+
+console.log('My favorite food is ' + objOne.favfood)
