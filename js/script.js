39b8f95be74b14d5b0c40739aa03dd4a18aeec5a
Stephanie Flora <twinsf2@gmail.com>
2020-11-02 19:59:34 -0800

    added comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-// Day 1
+// Day 1 take-home assignment
 let objOne = {
     name: 'Stephanie',
     favfood: "homemade peach ice cream",
 }
 
+/*
+A multiline commetn for the day 1 take home challenge
+*/
 console.log('My name is ' + objOne.name)
-
 console.log('My favorite food is ' + objOne.favfood)
