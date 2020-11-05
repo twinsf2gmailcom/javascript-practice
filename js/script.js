4bf022e5c2e995ee45c013c9c937027fd01b2fc8
Stephanie Flora <twinsf2@gmail.com>
2020-11-04 20:46:16 -0800

    day2 challenge

--- js/script.js
+++ js/script.js
@@ -7,7 +7,23 @@ let objOne = {
 /*
 A multiline commetn for the day 1 take home challenge
 */
-console.log('My name is ' + objOne.name)
-console.log('My favorite food is ' + objOne.favfood)
+// console.log('My name is ' + objOne.name)
+// console.log('My favorite food is ' + objOne.favfood)
+// console.log('the', 4.76, true)
+// console.log(typeof 'the',typeof 4,typeof true)
 
-debugger;
+// debugger;
+
+// DAY 2 --------------------------------------------------------
+console.log('Day 2 daily challenge ----------------------')
+let str1 = 'Supercalifragilisticexpialidocious';
+console.log('The lenght of the string ' + '"' + str1 + '" is ' + str1.length)
+console.log('Consol.log a number')
+let num1 = 143;
+console.log(num1)
+console.log('Consol.log a big integer')
+let bint1 = 143n;
+console.log(bint1)
+console.log('Check typeof the number and big integer----------')
+console.log('typeof for the number is ' + typeof num1)
+console.log('typeof for the big integer is ' + typeof bint1)
